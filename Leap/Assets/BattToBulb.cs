50979dc26e02e3339ae9d1486125a904139aebe2
Zachary Ferguson <zfergus2@gmu.edu>
2015-07-04 20:46:22 -0400

    Cleaned Up Code
    Cleaned Code and rebuilt for Unity 5

---
+++ Leap/Assets/BattToBulb.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BattToBuld : MonoBehaviour {
+
+	public float breakForce = 10.0f;
+	public float breakTorque = 10.0f;
+	//public Light light;
+	public bool connected, onTile;
+
+	// Use this for initialization
+	void Start () {
+		this.gameObject.GetComponent<Light>().intensity = 0;
+		this.connected = false; 
+		this.onTile = false;
+	}
+
+	void Update()
+	{
+		if (connected)
+			this.gameObject.GetComponent<Light>().intensity = 1.0f;
+		else if(!onTile){
+			this.gameObject.GetComponent<Light>().intensity = 0.0f;
+		}
+	}
+
+	void OnCollisionStay(Collision col)
+	{
+		if (col.gameObject.tag == "batt")
+		{
+			foreach (ContactPoint contact in col.contacts) 
+			{
+					Debug.DrawRay (contact.point, contact.normal, Color.white);
+			}
+			SpringJoint spring = GetComponent<SpringJoint> ();       
+			if (spring != null)
+				Destroy (spring);
+			FixedJoint head_joint = gameObject.AddComponent<FixedJoint> ();
+			head_joint.breakForce = breakForce;
+			head_joint.breakTorque = breakTorque;
+			this.gameObject.GetComponent<Light>().intensity = 1;
+			this.connected = true;
+			this.onTile = false;
+		} 
+		else if (col.gameObject.tag == "tile") 
+		{
+			this.onTile = true;
+		}
+		else 
+		{
+			this.connected = false;
+			this.onTile = false;
+		}
+	}
+}
---
+++ Leap/Assets/TileCharge.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TileCharge : MonoBehaviour
+{
+
+	bool charged;
+	public float intense, resistance;
+	GameObject go;
+
+	// Use this for initialization
+	void Start ()
+	{
+		this.intense = 10.0f; 
+		this.resistance = 1.0f;
+		this.charged = false;
+		this.go = null;
+	}
+
+	// Update is called once per frame
+	void Update ()
+	{
+		if (this.charged)
+		{
+			this.gameObject.GetComponent<Light>().intensity = 0.1f;
+			if(this.go!=null)
+			{ 
+				this.go.GetComponent<Light>().intensity = intense * resistance;
+			}
+		} 
+		else
+		{
+			this.gameObject.GetComponent<Light>().intensity = 0.0f;
+			if(this.go!=null)
+			{
+				go.GetComponent<Light>().intensity = 0.0f;
+			}
+		}
+	}
+
+	void OnCollisionEnter(Collision col)
+	{
+		if (col.gameObject.tag == "resis")
+		{
+			this.resistance = 0.5f;
+		}
+		else if (col.gameObject.tag == "resis2")
+		{
+			this.resistance = 0.75f;
+		}
+		else if (col.gameObject.tag == "batt")
+		{
+			this.charged = true;
+		}
+		else if (col.gameObject.tag == "bulb" && this.charged)
+		{
+			this.go = col.gameObject;
+			this.go.GetComponent<Light>().intensity = intense * resistance;
+		} 
+		else
+		{
+			this.charged = false;
+			this.resistance = 1.0f;
+		}
+	}
+	
+	void OnCollisionExit(Collision col)
+	{
+		if (col.gameObject.tag == "batt")
+		{
+			this.charged = false;
+			if(this.go != null)
+			{
+				this.go.GetComponent<Light>().intensity = 0.0f;
+			}
+		}
+		else if (col.gameObject.tag == "resis" || 
+				 col.gameObject.tag == "resis2")
+		{
+			this.resistance = 1.0f;
+		}
+	}
+}
